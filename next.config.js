17d5c231c7b773b6d01f2d8e74f437fba4d73eca
Book Chaowalit <bookchaowalit@gmail.com>
2026-06-18 00:45:18 +0700

    chore: update dependencies and build configuration

--- next.config.js
+++ next.config.js
@@ -59,6 +59,17 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'icon.horse',
+        pathname: '/icon/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.microlink.io',
+      },
+    ],
   },
   
   // Headers for caching and performance
